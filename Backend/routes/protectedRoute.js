7458d6194cd56329a50128771b4a7c9697d202b9
UmangKanzariya <umangkanzariya0@gmail.com>
2026-01-13 19:32:11 +0530

    Update protectedRoute.js

--- Backend/routes/protectedRoute.js
+++ Backend/routes/protectedRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../Middleware/authMiddleware");
 
 router.get("/protected", authMiddleware, (req, res) => {
   try {
